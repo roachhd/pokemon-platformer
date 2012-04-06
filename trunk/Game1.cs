d3f7628fa07760ae2fabde3f38df8ee23d8c81c6
senor.tubes@gmail.com <senor.tubes@gmail.com@ec2daa3f-75b9-11b7-916b-503e6ad25bcd>
2012-04-06 02:54:41 +0000

    basic game code with the ability to generate the ground matrix.

---
+++ trunk/Game1.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Pokemon
+{
+
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        public static Vector2[] TheMatrix;
+        Ground[] myGround = new Ground[3072];
+
+        public Vector2[] MatrixInit()
+        {   int counter=0;
+            int h = 2400;
+            int w = 800;
+            int x = 25; //something
+            /*Vector2[] result*/
+            TheMatrix = new Vector2[3072];
+            for (int i = 0; i < h; i+=x)
+            {
+                for (int j = 0; j < w; j += x)
+                {
+                    TheMatrix[counter] = new Vector2(i + (x / 2), j + (x / 2));
+                    counter++;
+                }
+            }
+
+            return TheMatrix;
+        }
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            MatrixInit();
+            for (int i = 0; i < TheMatrix.Length; i++)
+            {
+                Components.Add(
+                myGround[i] = new Ground(this, TheMatrix[i], 1)
+                );
+            }
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                this.Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.White);
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
