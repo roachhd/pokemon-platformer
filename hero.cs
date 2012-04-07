493b5f7274c7755a19864cdb2e2bb1e4489a3063
Emihaumut <Emihaumut@gmail.com>
2012-04-07 22:50:40 +0000

    Better correspondence with the character class

--- hero.cs
+++ hero.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -73,88 +73,9 @@ namespace pokemon
 
 
 
-        int step = 1;
-        int walked = 0, climbed = 0;
-        int counter = 0;
-        int ladder = 1;
-        int jumper = 0;
-        int percent = 400; 
-        int got;
+       
 
-        private bool collisionWithFloor() { return false; }
-
-        private void jump(GameTime gameTime)
-        {
-            float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            Position -= new Vector2(0, elapsed) * percent;
-            percent -= 10;
-
-            if (collisionWithFloor())
-            {
-                jumping = 0;
-                percent = 400;
-            }
-
-        }
-
-        /*
-         * Steps:
-         * 1 to 4: walking right
-         * -1 to -4: walking left
-         * 6 to 9: climbing up
-         * -6 to -9: climbing down
-         */
-
-        private Rectangle walk(GameTime gameTime)
-        {
-
-            float elapsed = (float)gameTime.ElapsedGameTime.TotalMilliseconds;
-            Rectangle rec;
-            counter++;
-
-            if (counter > 5)
-            {
-                counter = 0;
-                if (walked == 1)
-                {
-                    walked = 0;
-                    if (step == 4) { step = 1; }
-                    else { step++; }
-                }
-                else if (walked == -1)
-                {
-                    walked = 0;
-                    if (step == -4) { step = -1; }
-                    else { step--; }
-                }
-                if (climbed == 1)
-                {
-                    climbed = 0;
-                    if (step == 9) { step = 6; }
-                    else { step++; }
-                }
-            }
-
-            //walking steps
-            if (step == 1) rec = new Rectangle(0, 0, 64, 64);
-            else if (step == 2) rec = new Rectangle(64, 0, 64, 64);
-            else if (step == 3) rec = new Rectangle(128, 0, 64, 64);
-            else if (step == 4) rec = new Rectangle(64, 0, 64, 64);
-            else if (step == -1) rec = new Rectangle(0, 64, 64, 64);
-            else if (step == -2) rec = new Rectangle(64, 64, 64, 64);
-            else if (step == -3) rec = new Rectangle(128, 64, 64, 64);
-            else if (step == -4) rec = new Rectangle(64, 64, 64, 64);
-            //climbing steps
-            else if (step == 6) rec = new Rectangle(0, 128, 64, 64);
-            else if (step == 7) rec = new Rectangle(64, 128, 64, 64);
-            else if (step == 8) rec = new Rectangle(128, 128, 64, 64);
-            else rec = new Rectangle(64, 128, 64, 64);
-
-            return rec;
-        }
-
-        int jumping;
+       
         public override void Update(GameTime gameTime)
         {
             float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
