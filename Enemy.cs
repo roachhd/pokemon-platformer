169a32027a4a2c3245581cd1f658539b1cda89b0
trmcami <trmcami@gmail.com>
2012-04-12 00:07:21 +0000

    enemy with gravity

--- Enemy.cs
+++ Enemy.cs
@@ -10,10 +10,12 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
-namespace Pokemon
+namespace pokemon
 {
     class Enemy : Character
     {
+        int sprwid=64, sprhei=64;
+
         hero mage;
         public Rectangle boundingBox { get; set; }
 
@@ -24,7 +26,7 @@ namespace Pokemon
 
             Sprite = Game.Content.Load<Texture2D>("enemy");
 
-            Vector2 offset = new Vector2(Sprite.Width / 2, Sprite.Height / 2);
+            Vector2 offset = new Vector2(sprwid / 2, sprhei / 2);
             Position = Position - offset;
 
             spriteBatch = new SpriteBatch(Game.GraphicsDevice);
@@ -34,9 +36,61 @@ namespace Pokemon
         public override void Update(GameTime gameTime)
         {
 
-            boundingBox = new Rectangle((int)Position.X, (int)Position.Y, 64, 64);
+            boundingBox = new Rectangle((int)Position.X, (int)Position.Y, 50, 50);
             float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
             KeyboardState k = Keyboard.GetState();
+
+
+            foreach (var c in Game.Components)
+            {
+                Ground b = c as Ground;
+                //put this in the enemy class
+                if (b != null)
+                {
+                    if (boundingBox.Intersects(b.bb) && ((b.type == 1) || (b.type == 2) || (b.type == 3)))
+                    {
+                        if ((Position.Y > (b.bb.Top - 39)) && (Position.Y < (b.bb.Bottom - 39))) // if the main player's position is between the top and the bottom of the bounding box
+                        {
+                            //it's a side collision.  check if position is to the left or right, and handle appropriately
+                            if (Position.X < b.position.X) //it's on the left
+                            {
+                                Position = new Vector2(b.bb.Left - boundingBox.Width, Position.Y);
+                            }
+                            else Position = new Vector2(b.bb.Right, Position.Y);
+                        }
+                        else if (Position.Y < b.bb.Top) // if the position of the player is above the top of the box, it's an above collision
+                        {
+                            Position = new Vector2(Position.X, b.bb.Top - boundingBox.Height + 1);
+                        }
+                        else Position = new Vector2(Position.X, b.bb.Bottom); //else it hit the bottom, but this should never happen.
+                    }
+                    //ladders
+                    /*if (boundingBox.Contains(b.bb) || (boundingBox.Intersects(b.bb)) && (b.type == 4))
+                    {
+                        if (k.IsKeyDown(Keys.Up))
+                        {
+                            if ((step >= 0 && step <= 4) || (step < 0)) //if he had a walking sprite
+                                step = 6;
+                            Position -= new Vector2(0, elapsed) * 80;
+                            climbed = 1;
+                        }
+                        if (k.IsKeyDown(Keys.Down))
+                        {
+                            if ((step >= 0 && step <= 4) || (step < 0)) //if he had a walking sprite
+                                step = 6;
+                            Position += new Vector2(0, elapsed) * 80;
+                            climbed = 1;
+                        }
+                    }*/
+                    if (boundingBox.Intersects(b.bb) && (b.type == 0) && (b.position.Y > (Position.Y - 64)))
+                    {
+                        Position += new Vector2(0, elapsed) * 80;
+
+                    }
+                }
+
+            }
+
 
             if (Position.X > mage.Position.X)
             {
