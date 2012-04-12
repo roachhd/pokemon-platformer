3d4b2a9c112c1e201fad3eabbba35100f978a13c
senor.tubes@gmail.com <senor.tubes@gmail.com@ec2daa3f-75b9-11b7-916b-503e6ad25bcd>
2012-04-12 04:58:45 +0000

    fixed it so the character can properly climb ladders.

--- trunk/hero.cs
+++ trunk/hero.cs
@@ -26,7 +26,13 @@ namespace Pokemon
         int score;
 
         int lastWalk = 1; int attacked = 0;
-        Rectangle boundingBox;
+        Rectangle boundingBox
+        {
+            get
+            {
+                return new Rectangle((int)Position.X, (int)Position.Y, 50, 50);
+            }
+        }
         Game g;
 
 
@@ -161,7 +167,7 @@ namespace Pokemon
                 attacked = 0;
             }
 
-            
+
 
             if (k.IsKeyDown(Keys.Left))
             {
@@ -189,7 +195,7 @@ namespace Pokemon
 
             IGameComponent removeMe = null;
             IGameComponent removeMe2 = null;
-            boundingBox = new Rectangle((int)Position.X, (int)Position.Y, 50, 50);
+            
             //Collision Detection (for stones)
             foreach (var c in Game.Components)
             {
@@ -209,7 +215,7 @@ namespace Pokemon
                     if (boundingBox.Intersects(b.bb) && ((b.type == 1) || (b.type == 2) || (b.type == 3)))
                     {
                         if ((Position.Y > (b.bb.Top - 39)) && (Position.Y < (b.bb.Bottom - 39)))
-                            // if the main player's position is between the top and the bottom of the bounding box
+                        // if the main player's position is between the top and the bottom of the bounding box
                         {
                             //it's a side collision.  check if position is to the left or right, and handle appropriately
                             if (Position.X < b.position.X) //it's on the left
@@ -219,35 +225,38 @@ namespace Pokemon
                             else Position = new Vector2(b.bb.Right, Position.Y);
                         }
                         else if (Position.Y < b.bb.Top)
-                            // if the position of the player is above the top of the box, it's an above collision
+                        // if the position of the player is above the top of the box, it's an above collision
                         {
                             Position = new Vector2(Position.X, b.bb.Top - boundingBox.Height + 1);
                         }
+                        
                         else
                             Position = new Vector2(Position.X, b.bb.Bottom);
-                                //else it hit the bottom, but this should never happen.
+                        //else it hit the bottom, but this should never happen.
                     }
 
                     if ((boundingBox.Contains(b.bb) || (boundingBox.Intersects(b.bb))) && (b.type == 4))
+                    //    if (boundingBox.Contains(b.bb) && (b.type == 4))
                     {
+                        Position = new Vector2(Position.X, Position.Y - 2);
                         if (k.IsKeyDown(Keys.Up))
                         {
                             if ((step >= 0 && step <= 4) || (step < 0)) //if he had a walking sprite
                                 step = 6;
-                            Position -= new Vector2(0, elapsed)*160;
+                            Position -= new Vector2(0, elapsed) * 50;
                             climbed = 1;
                         }
                         if (k.IsKeyDown(Keys.Down))
                         {
                             if ((step >= 0 && step <= 4) || (step < 0)) //if he had a walking sprite
                                 step = 6;
-                            Position += new Vector2(0, elapsed)*80;
+                            Position += new Vector2(0, elapsed) * 80;
                             climbed = 1;
                         }
                     }
                     if (boundingBox.Intersects(b.bb) && (b.type == 0) && (b.position.Y > (Position.Y - 64)))
                     {
-                        Position += new Vector2(0, elapsed)*80;
+                        Position += new Vector2(0, elapsed) * 80;
 
                     }
                 }
