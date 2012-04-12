1d4f6693c1ea20e9358c28bc1b27146df8db351a
trmcami <trmcami@gmail.com>
2012-04-12 01:55:16 +0000

    new attack class! oh by the way i forgot to say that the new enemy class has attack collision. Also, stronger attacks get 100% power, same element gets 50% and weaker gets 25%

--- Attack.cs
+++ Attack.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,58 +21,68 @@ namespace pokemon
 
         Texture2D Sprite;
         SpriteBatch spriteBatch;
+        int sprW=33, sprH=33;
+        public Rectangle boundingBox;
+        Rectangle spr;
 
-        public Attack(Game g,Vector2 position, int type, int dir) : base(g){
+        public Attack(Game g, Vector2 position, int type, int dir)
+            : base(g)
+        {
             Type = type;
             Dir = dir;
             if (Dir < 0) Position = position + new Vector2(-10, 40);
             else Position = position + new Vector2(65, 40);
-            
+
 
         }
 
         protected override void LoadContent()
         {
+            Sprite = Game.Content.Load<Texture2D>("attack");
+            spriteBatch = new SpriteBatch(Game.GraphicsDevice);
             if (Type == 1)
             {
-                Sprite = Game.Content.Load<Texture2D>("red");
-                spriteBatch = new SpriteBatch(Game.GraphicsDevice);
-                
+                spr = new Rectangle(0,0,33,33);
+
             }
             else if (Type == 2)
             {
-                Sprite = Game.Content.Load<Texture2D>("blue");
-                spriteBatch = new SpriteBatch(Game.GraphicsDevice);
+                spr = new Rectangle(33, 0, 33, 33);
             }
-            else {
-                Sprite = Game.Content.Load<Texture2D>("yellow");
-                spriteBatch = new SpriteBatch(Game.GraphicsDevice);
+            else
+            {
+                spr = new Rectangle(66, 0, 33, 33);
             }
 
-            Vector2 offset = new Vector2(Sprite.Height / 2, Sprite.Width / 2); //is this ok??? or first width then height
+            Vector2 offset = new Vector2(sprW / 2, sprH / 2); //is this ok??? or first width then height
             Position = Position - offset;
+
             
         }
 
         public override void Update(GameTime gameTime)
         {
             float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
+             boundingBox=new Rectangle((int)Position.X, (int)Position.Y, sprW, sprH);
             //Console.WriteLine("dir: " + Dir);
             if (Dir < 0)
             { //left
                 Position -= new Vector2(elapsed, 0) * 100;
             }
-            else {
+            else
+            {
                 Position += new Vector2(elapsed, 0) * 100;
             }
             base.Update(gameTime);
         }
 
+        
+
         public override void Draw(GameTime gameTime)
         {
-            
-            spriteBatch.Begin();
-            spriteBatch.Draw(Sprite, Position, Color.White);
+
+            spriteBatch.Begin(); 
+            spriteBatch.Draw(Sprite, Position, spr, Color.White, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
             spriteBatch.End();
 
             base.Draw(gameTime);
